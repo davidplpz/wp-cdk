79c6c4d298ac256c1ffb2eafa535c77aed91730f
Davidpl <dvpl88@gmail.com>
2023-09-22 09:39:42 +0200

    using new versions

--- lib/wordpress_cdk-stack.ts
+++ lib/wordpress_cdk-stack.ts
@@ -1,7 +1,6 @@
 import * as ec2 from "@aws-cdk/aws-ec2"; // import ec2 library
 import * as iam from "@aws-cdk/aws-iam";
-import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
+import * as cdk from "@aws-cdk/core";
 import "dotenv/config";
 import * as fs from "fs";
 
@@ -12,20 +11,18 @@ const config = {
   },
 };
 export class WordpressCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, { ...props, env: config.env });
 
-    const vpc = ec2.Vpc.fromLookup(this, process.env.VPC_NAME, {
-      isDefault: true,
-    });
+    const vpc = new ec2.Vpc(this, process.env.VPC_NAME || "VPC", {});
 
-    const role = new iam.Role(this, process.env.ROL_NAME, {
+    const role = new iam.Role(this, process.env.ROL_NAME || "ROLE", {
       assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
     });
 
     const securityGroup = new ec2.SecurityGroup(
       this,
-      process.env.SECURITY_ROL_NAME,
+      process.env.SECURITY_ROL_NAME || "SecurityGroup",
       {
         vpc: vpc,
         allowAllOutbound: true,
@@ -51,33 +48,40 @@ export class WordpressCdkStack extends cdk.Stack {
       "Allows HTTPS access from Internet"
     );
 
-    const ec2Instance = new ec2.Instance(this, "simple-instance-1", {
-      vpc: vpc,
-      role: role,
-      securityGroup: securityGroup,
-      instanceName: process.env.EC2_NAME,
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T2,
-        ec2.InstanceSize.MICRO
-      ),
-      machineImage: ec2.MachineImage.latestAmazonLinux({
-        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-      }),
-      keyName: process.env.EC2_KEY,
-    });
+    const ec2Instance = new ec2.Instance(
+      this,
+      process.env.EC2_NAME || "Ec2Instance",
+      {
+        vpc: vpc,
+        role: role,
+        securityGroup: securityGroup,
+        instanceName: process.env.EC2_NAME,
+        instanceType: ec2.InstanceType.of(
+          ec2.InstanceClass.T2,
+          ec2.InstanceSize.MICRO
+        ),
+        machineImage: ec2.MachineImage.latestAmazonLinux({
+          generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+        }),
+        keyName: process.env.EC2_KEY,
+      }
+    );
 
-    const eip = new ec2.CfnEIP(this, process.env.SERVER_IP_NAME, {
+    const eip = new ec2.CfnEIP(this, process.env.SERVER_IP_NAME || "EIP", {
       instanceId: ec2Instance.instanceId,
     });
 
-    new ec2.CfnEIPAssociation(this, process.env.ELASTICA_IP_NAME, {
-      eip: eip.ref,
-      instanceId: ec2Instance.instanceId,
-    });
+    new ec2.CfnEIPAssociation(
+      this,
+      process.env.ELASTICA_IP_NAME || "ElasticaIP",
+      {
+        eip: eip.ref,
+        instanceId: ec2Instance.instanceId,
+      }
+    );
 
     //TODO - Crear las claves para el acceso SSH
 
-    //TODO - Generar script para montar el tinglado
     ec2Instance.addUserData(fs.readFileSync("lib/mount.sh", "utf8"));
 
     new cdk.CfnOutput(this, "simple-instance-1-output", {
